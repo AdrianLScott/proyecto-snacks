e5348593fae7f719d835abcb1685891fdccf7d5c
CesarCedillo97 <32661803+CesarCedillo97@users.noreply.github.com>
2018-11-26 10:30:47 -0700

    Revert "Revert "solución de bugs 4, 5 y 6""
    This reverts commit 5272cd682d312ead789f4a49e2521bee997cdc39.

--- src/pages/product-details/product-details.ts
+++ src/pages/product-details/product-details.ts
@@ -49,19 +49,19 @@ export class ProductDetailsPage {
 
 	increment() {
 		if (this.currentNumber >= 20) {
-			this.total=this.currentNumber*this.producto.precio;
+			this.total=(this.currentNumber*this.producto.precio).toFixed(2);
 		}else{
   			this.currentNumber++;
-			this.total=this.currentNumber*this.producto.precio;
+			this.total=(this.currentNumber*this.producto.precio).toFixed(2);
 		}
 	}
 
 	decrement() {
 		if (this.currentNumber <= 1) {
-			this.total=this.currentNumber*this.producto.precio;
+			this.total=(this.currentNumber*this.producto.precio).toFixed(2);
 		}else{
 			this.currentNumber--;
-			 	this.total=this.currentNumber*this.producto.precio;
+			 	this.total=(this.currentNumber*this.producto.precio).toFixed(2);
 		}
 		
 	}
@@ -152,7 +152,7 @@ export class ProductDetailsPage {
 
 				this.toast.showWithOptions(
 					{
-					  message: "Pedido modificado",
+					  message: "Agregado exitosamente al carrito",
 					  duration: 2000,
 					  position: 'bottom',
 					  addPixelsY: -80  // added a negative value to move it up a bit (default 0)
